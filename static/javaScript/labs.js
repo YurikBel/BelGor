0c9ed249a14e84409fb4c7598791894021fa886d
Yurik <belozerov.yurik@yandex.ru>
2022-10-05 22:45:36 +0300

    Добавил адаптацию сайта

--- static/javaScript/labs.js
+++ static/javaScript/labs.js
@@ -7,12 +7,21 @@ function init(){
 
 
 function changer(){
-    let list = document.getElementsByClassName('elem');
-    for (let b of list){
-        b.onclick = function(){
-            const href = this.getAttribute('data-href-file')
-            const preview = document.getElementById('preview')
-            preview.setAttribute('src', href)
+    let list = document.getElementsByClassName('link');
+    if( window.innerWidth >= 700 ){
+        for (let b of list){
+            b.onclick = function(){
+                const href = this.getAttribute('data-href-file')
+                const preview = document.getElementById('preview')
+                preview.setAttribute('src', href)
+            }
+        }
+    }
+    else{
+        for (let b of list){
+            const href = b.getAttribute('data-href-file')
+            b.setAttribute('href', href)
+            b.setAttribute('download', 'newfilename')
         }
     }
 }
