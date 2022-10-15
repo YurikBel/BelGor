b54d775e99376823d2172a4f89c08ff8b242b348
Yurik <belozerov.yurik@yandex.ru>
2022-10-15 16:38:46 +0300

    добавил выпад списки

--- static/javaScript/test.js
+++ static/javaScript/test.js
@@ -1,26 +1,25 @@
-var cards = $('#card-slider .slider-item').toArray();
-
-startAnim(cards);
-
-function startAnim(array){
-    if(array.length >= 4 ) {
-        TweenMax.fromTo(array[0], 0.5, {x:0, y: 0, opacity:0.75}, {x:0, y: -120, opacity:0, zIndex: 0, delay:0.03, ease: Cubic.easeInOut, onComplete: sortArray(array)});
-
-        TweenMax.fromTo(array[1], 0.5, {x:79, y: 125, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.75, zIndex: 0, boxShadow: '-5px 8px 8px 0 rgba(82,89,129,0.05)', ease: Cubic.easeInOut});
-
-        TweenMax.to(array[2], 0.5, {bezier:[{x:0, y:250}, {x:65, y:200}, {x:79, y:125}], boxShadow: '-5px 8px 8px 0 rgba(82,89,129,0.05)', zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
-
-        TweenMax.fromTo(array[3], 0.5, {x:0, y:400, opacity: 0, zIndex: 0}, {x:0, y:250, opacity: 0.75, zIndex: 0, ease: Cubic.easeInOut}, );
-    } else {
-        $('#card-slider').append('<p>Sorry, carousel should contain more than 3 slides</p>')
-    }
-}
-
-function sortArray(array) {
-    clearTimeout(delay);
-    var delay = setTimeout(function(){
-        var firstElem = array.shift();
-        array.push(firstElem);
-        return startAnim(array);
-    },3000)
-}
+jQuery(document).ready(function($) {
+        		"use strict";
+        		//  TESTIMONIALS CAROUSEL HOOK
+		        $('#customers-testimonials').owlCarousel({
+		            loop: true,
+		            center: true,
+		            items: 3,
+		            margin: 0,
+		            autoplay: true,
+		            dots:true,
+		            autoplayTimeout: 15000,
+		            smartSpeed: 450,
+		            responsive: {
+		              0: {
+		                items: 1
+		              },
+		              768: {
+		                items: 2
+		              },
+		              1170: {
+		                items: 2
+		              }
+		            }
+		        });
+        	});
